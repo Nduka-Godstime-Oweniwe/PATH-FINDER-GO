93a05f1f918d6a30ec30a566ce3623fbc52ad931
Nduka Oweniwe <oweniwenduka@gmail.com>
2026-05-10 01:35:45 +0200

    Worked on the main.go file

--- main.go
+++ main.go
@@ -1,5 +1,25 @@
 package main
 
+import (
+	PathFinder "PathFinder/functions"
+	"os"
+	"os/exec"
+	"runtime"
+	"strings"
+)
+
+func clearScreen() {
+	switch runtime.GOOS {
+	case "windows":
+		cmd := exec.Command("cmd", "/c", "cls")
+		cmd.Stdout = os.Stdout
+		cmd.Run()
+	default:
+		cmd := exec.Command("clear")
+		cmd.Stdout = os.Stdout
+		cmd.Run()
+	}
+}
 func main() {
 	// slice := []string{
 	// 	"..S..X",
@@ -18,5 +38,31 @@ func main() {
 	// 	"...............................................",
 	// 	"..............................................E",
 	// }
+	for {
+		clearScreen()
+		userSlice := PathFinder.UserInput()
+		board := PathFinder.CreateBoard(userSlice)
+		// row, col := PathFinder.FindS(board)
+		for {
+			clearScreen()
+			if PathFinder.UserChoice(board) {
+				continue
+			} else {
+				break
+			}
+		}
+		replay := ""
+		for replay != "n" {
+			replay = strings.ToLower(PathFinder.Input("Do you want to replay? (y/n): "))
+			if strings.ToLower(replay) == "y" {
+				break
+			} else if strings.ToLower(replay) == "n" {
+				return
+			} else {
+				println("Invalid Input. Type 'y' for yes or 'n' for no.")
+			}
+
+		}
+	}
 
 }
